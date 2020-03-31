4db17f401a68a8512d504dee619131b1c656a4ab
riyadzaigirdar <riyadzaigir280@gmail.com>
2020-03-31 15:23:36 +0600

    userlistchangeadmin individualuser

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,32 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        email = 'riyad@gmail.com'
+        password = '1234'
+
+        user = get_user_model().objects.create_user(
+        email = email,
+        password = password
+        )
+        self.assertEqual(user.email,email)
+        self.assertTrue(user.check_password(password))
+
+    def test_create_new_user(self):
+        email = 'riyad@ASD.com'
+        user = get_user_model().objects.create_user(email,'1234')
+        self.assertEqual(user.email,email.lower())
+
+    def test_new_user_invalid_email(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None,'1234')
+
+    def test_super_user(self):
+        user = get_user_model().objects.create_superuser(
+        'riyad@gmail.com',
+        '1234'
+        )
+        self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)
